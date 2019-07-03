98fb351f8d0e92420fe720f3a1e8335376d035e9
halynaburdiian <verdamtion@gmail.com>
2019-07-03 10:54:36 +0200

    Create elements with data from form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -1,6 +1,16 @@
 function submitComment() {
     const inputField = document.getElementById('name')
-    const name = inputField.value;
-    const msg = textArea.value;
-    console.log(msg);
+    const name = inputField.value
+    const textArea = document.getElementById('msg')
+    const msg = textArea.value
+    const comment = document.createElement('section')
+    const h4 = document.createElement('h4')
+    const p = document.createElement('p')
+    h4.innerHTML = `${name} said:`
+    p.innerHTML = msg
+    comment.classList.add('comment')
+    comment.appendChild(h4)
+    comment.appendChild(p)
+
+  console.log(comment)
 }
