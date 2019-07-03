350fe56859eb88cb6c8d68d34e0389fbf075986a
halynaburdiian <verdamtion@gmail.com>
2019-07-03 10:57:54 +0200

    Display elements and reset form

--- pages/detail-page.js
+++ pages/detail-page.js
@@ -11,6 +11,8 @@ function submitComment() {
     comment.classList.add('comment')
     comment.appendChild(h4)
     comment.appendChild(p)
-
-  console.log(comment)
+    const commentSection = document.getElementById('comments')
+    commentSection.appendChild(comment)
+    inputField.value = null
+    textArea.value = null
 }
